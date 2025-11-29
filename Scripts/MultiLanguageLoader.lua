67b83f8ed74b61a9ad932091c1c9a1aec1c6a469
Jai <jyai.san+acs@gmail.com>
2025-11-30 01:39:47 +0800

    Update MultiLanguageLoader.lua

--- Scripts/MultiLanguageLoader.lua
+++ Scripts/MultiLanguageLoader.lua
@@ -2,8 +2,8 @@ MultiLanguage = MultiLanguage or {}
 
 function MultiLanguage:OnInit()
 	xlua.private_accessible(CS.TFMgr)
-	-- self.language = CS.TFMgr.Instance.Language
-	self.language = "en"
+	self.language = CS.TFMgr.Instance.Language
+	-- self.language = "en"
 
 	local folderPath = CS.ModsMgr.Instance:GetFilePath("Language/Mods/" .. "Jai_Test" .. "/" .. self.language .. ".txt", "Jai_Test")
 	CS.TFMgr.Instance:LoadLangKvFile(folderPath)
