f8a96e67ec19845f09f3efc065162a970b77e7a4
Jai <jyai.san+acs@gmail.com>
2025-11-30 21:13:36 +0800

    Update MultiLanguageLoader.lua

--- Scripts/MultiLanguageLoader.lua
+++ Scripts/MultiLanguageLoader.lua
@@ -1,10 +1,13 @@
 MultiLanguage = MultiLanguage or {}
 
-function MultiLanguage:OnInit()
+function MultiLanguage:Load(mod)
 	xlua.private_accessible(CS.TFMgr)
-	self.language = CS.TFMgr.Instance.Language
-	-- self.language = "en"
+	local language = CS.TFMgr.Instance.Language
 
-	local folderPath = CS.ModsMgr.Instance:GetFilePath("Language/Mods/" .. "Jai_Test" .. "/" .. self.language .. ".txt", "Jai_Test")
+	if language == "cn" then
+		return -- Default untranslated text should already be in chinese
+	end
+
+	local folderPath = CS.ModsMgr.Instance:GetFilePath("Language/Mods/" .. mod .. "/" .. language .. ".txt", mod)
 	CS.TFMgr.Instance:LoadLangKvFile(folderPath)
 end
